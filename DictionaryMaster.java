62f0a1c999059e5beec9b810bf146006b7e13bcf
roberthandjordan <rhj2002@columbia.edu>
2014-04-22 21:56:04 -0400

    Adding files DictionaryMaster and README as a text file

---
+++ DictionaryMaster.java
@@ -0,0 +1,67 @@
+//****************************************************************************
+//Robert Jordan
+//rhj2002@columbia.edu
+//This file contains the Dictionary Master Class of the program for the 
+//Scrabble Dictionary Checker assignment.
+//
+//Java Version 6
+//****************************************************************************
+
+public class DictionaryMaster {
+	// instance variables
+
+	public DictionaryMaster() {
+		//
+	}
+	//default constructor
+
+	public DictionaryMaster(String filename) {
+		//
+	}
+	//constructor with parameters
+
+	//
+
+	public void launch(String filename) {
+		//
+	}
+	//
+
+	public void target(int g, int h, char letter) {
+		//
+	}
+	//
+
+	public writeLists(String[] results) {
+		//toString method?
+	}
+	//
+
+	public printLists(String[] results) {
+
+	}
+	//
+
+	public void someMethod() {
+		//method?
+	}
+	//write this out so it makes a file for each case that you can save
+	//so for vowelheavy, it should kick out a file called
+	//vowelheavy5x
+	//for vowelheavy function, 5 letters in length, featuring letter x
+
+	/*
+	try {
+		//
+	}
+	catch (IOException e) {
+		//
+	}
+	catch (ArrayIndexOutOfBoundsException e) {
+		//
+	}
+	*/
+
+}
+
+//****************************************************************************
