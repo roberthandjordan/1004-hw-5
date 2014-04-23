a1d8f05e74795229b0830de260e58c772abfd94d
roberthandjordan <rhj2002@columbia.edu>
2014-04-22 21:48:22 -0400

    One Try to Commit to Git

--- WordLists.java
+++ WordLists.java
@@ -1,15 +1,112 @@
-//******************************
-// Robert Jordan
-// 
-
+//****************************************************************************
+//Robert Jordan
+//rhj2002@columbia.edu
+//This file contains the Words Lists Class of the program for the Scrabble 
+//Dictionary Checker assignment.
+//
+//Java Version 6
+//****************************************************************************
 
 public class WordLists {
-	// instance variables
+	String fileChoice;
+	//instance variables
 
-	// constructor
-	public WordLists(String filename) {
-		
+	public WordLists() {
+		fileChoice = "dictionary.txt";
+
+		File inFile = new File(args[0]);
+		Scanner dataIn = new Scanner(inFile);
+		String fileEntry;
+		PrintWriter dataOut = new PrintWriter(args[1]);
+		while (dataIn.hasNext()) {
+			fileEntry = dataIn.next();
+			dataOut.println(fileEntry);
+		}
+			dataOut.close();
+
+		//
+	}
+	//default constructor	
+
+	public WordLists(String fileName) {
+		fileChoice = fileName;
+
+		File inFile = new File(args[0]);
+		Scanner dataIn = new Scanner(inFile);
+		String fileEntry;
+		PrintWriter dataOut = new PrintWriter(args[1]);
+		while (dataIn.hasNext()) {
+			fileEntry = dataIn.next();
+			dataOut.println(fileEntry);
+		}
+			dataOut.close();
+
+		//
+	}
+	//constructor with parameters
+
+	public printLists(String[] results) {
+
+	}
+	//
+
+	public writeLists(String[] results) {
+		//toString method?
+	}
+	//
+
+	public void launch(String[] fileName) {
+		//
 	}
 
+	public void target(int g, int h, char letter) {
+		//
+	}
+	//
+
+	public String[] lengthN(int n) {
+		//
+	}
+	//
+
+	public String[] startsWith(int n, char firstLetter) {
+		//local variable?
+		//firstLetter
+	}
+	//
+
+	public String[] containsLetter(int n, char included) {
+
+	}
+	//
+
+	public String[] vowelHeavy(int n, int m) {
+		//toString method?
+	}
+	//
+
+	public String[] multiLetter(int m, char included) {
+
+	}
+	//
+
+	public void someMethod() {
+		//what?
+	}
+	//
+
+	/*
+	try {
+		//
+	}
+	catch (IOException e) {
+		//
+	}
+	catch (ArrayIndexOutOfBoundsException e) {
+		//
+	}
+	*/
 	
 }
+
+//****************************************************************************
--- WordListsTester.java
+++ WordListsTester.java
@@ -1,12 +1,65 @@
-//******************************
-// Robert Jordan
-// 
+//****************************************************************************
+//Robert Jordan
+//rhj2002@columbia.edu
+//This file contains the Words List Tester Class and is the tester of the 
+//program for the Scrabble Dictionary Checker assignment.
+//
+//Java Version 6
+//****************************************************************************
 
 
 public class WordListsTester {
 
 	public static void main(String[] args) {
-		
+		int again = 0;
+		//again initialized as zero for use in do while loop so that it will 
+		//execute once, but will exit unless otherwise prompted
+
+		System.out.println("Welcome to the Scabble Dictionary Checker!!!!!"
+			+ \n "This program will offer you a set of options for generating "
+			+ "words.  You can choose between a number of functions, "
+			+ "the results of which will be written out to a file.");
+
+
+		do {
+			if (args.length>0) {
+				WordLists portal = new WordLists();
+				portal.launch();
+
+
+			}
+			else {
+				WordLists portal = new WordLists(args);
+				portal.launch(args);
+
+
+			}
+
+
+			System.out.println("Would you like to try again?"
+				+ \n "Please enter 1 for yes or 0 for no.");
+
+			//try-catch construct to detect user choice to repeat or not
+			try {
+			Scanner trial = new Scanner(System.in);
+			again = trial.nextInt();
+			}
+			catch (IOException e) {
+				//
+				System.out.println("Please enter only either 1 for yes or "
+					+ "0 for no.");
+				Scanner retrial = new Scanner(System.in);
+				again = retrial.nextInt();
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				//
+			}
+			
+			//condition of do-while loop
+		} while (again == 1);
+
 	}
 
 }
+
+//****************************************************************************
